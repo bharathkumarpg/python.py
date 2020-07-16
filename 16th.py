4fb984d199c60cae353bf73d5ad12be057fd88ec
bharathkumarpg <57790987+bharathkumarpg@users.noreply.github.com>
2020-07-16 15:03:49 +0530

    Add files via upload

---
+++ 16th.py
@@ -0,0 +1,8 @@
+number1 = int(input("please enter the first number"))
+number2 = int(input("please enter the second number"))
+if number1<number2:
+    result = number2-number1
+    print(f"{result}")
+else:
+    result1 = number1-number2
+    print(f"{result1}")
